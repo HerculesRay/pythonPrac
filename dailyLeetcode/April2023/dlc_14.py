ef7d1555d910124b2ae3ebd3a01036697b0e1306
Rajiv Ray <rajivray@Rajivs-MacBook-Air.local>
2023-04-15 18:00:26 +0530

    test_dlc_push

---
+++ dailyLeetcode/April2023/dlc_14.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestCommonSeq(self, ostr, rstr):
+
+        m = len(ostr) + 1
+        n = len(rstr) + 1
+
+        dp = []
+
+        for i in range(m):
+            dp.append([0] * n)
+
+
+        for i in range(1, m):
+            for j in range(1, n):
+                beval = max(dp[i - 1][j], dp[i][j - 1])
+                dival = dp[i - 1][j - 1]
+                if rstr[i - 1] == ostr[j - 1]:
+                    dival += 1
+                dp[i][j] = max(dival, beval)
+        return dp[m - 1][n - 1]
+
+    def longestPalindromeSubseq(self, s: str) -> int:
+
+        return self.longestCommonSeq(s, s[::-1])
+
+sol = Solution()
+
+print(sol.longestPalindromeSubseq("bbbab"))
+
